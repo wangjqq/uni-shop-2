d9176b16a974955ab9a43fc143ca5bf622942692
WJQ <1092229612@qq.com>
2022-10-18 21:52:15 +0800

    完成了商品列表页面

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    Goods_list
+    <view class="goods-list">
+      <view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+        <my-goods :goods="goods"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,8 +12,57 @@
   export default {
     data() {
       return {
-
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        }, //请求参数对象
+        goodsList: [], //商品列表
+        total: 0, //商品总数
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png', //默认的空图片
+        isloading: false //是否正在请求数据
       };
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) {
+        return uni.$showMsg('人家是有底线的!')
+      }
+      if (this.isloading === false) {
+        this.queryObj.pagenum += 1
+        this.getGoodsList()
+      }
+
+    },
+    onPullDownRefresh() {
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    },
+    methods: {
+      // 获取商品列表
+      async getGoodsList(cb) {
+        this.isloading = true
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        cb && cb() //请求完毕就会按需调用cb回调函数
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+        this.isloading = false
+      },
+      gotoDetail(goods) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+        })
+      }
     }
   }
 </script>
